c0da553987ebbc8b7926a0209ee2665d2e4cc7b8
catsarecute-arnav <at9170@srmist.edu.in>
2025-09-13 23:11:29 +0530

    Create ArmStrong.py

---
+++ ArmStrong.py
@@ -0,0 +1,19 @@
+def armstrong():
+    try:
+        num=int(input("Enter a number to check if it's an Armstrong number: "))
+        if num<0:
+            print(f"{num} is not an Armstrong number.")
+            return
+        str_num=str(num)
+        len_str_num=len(str_num)
+        armstrong_sum=0
+        for i in str_num:
+            digit=int(i)
+            armstrong_sum+=digit**len_str_num
+        if armstrong_sum==num:
+            print(f"{num} is an Armstrong number.")
+        else:
+            print("Your arms are not strong :(")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+if __name__=="__main__":armstrong()
